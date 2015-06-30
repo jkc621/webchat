d4c0b9f59f821ddd1f147d12637bc092de47172e
jkc621 <jameskitamirike@gmail.com>
2015-06-30 15:58:26 +0200

    Added LESS Compilation through Gulp

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var gulp = require('gulp'); 
+var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var reload = browserSync.reload;
+// var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var rename = require('gulp-rename');
+// var livereload = require('gulp-livereload');
+
+gulp.task('browser-sync-dev', function(){
+	browserSync({
+		// server:{
+		// 	baseDir:"./build"
+		// }
+		proxy: "localhost:3000"
+	});
+	gulp.watch('css/*.less', ['less-dev']);
+	gulp.watch('js/*.js', ['scripts']);
+	gulp.watch("build/*.html").on('change', reload);
+	
+});
+
+gulp.task('less-dev', function() {
+    return gulp.src('css/main.less')
+    	.pipe(plumber())
+        .pipe(less())
+		.pipe(autoprefixer({
+			cascade: true
+		}))
+        .pipe(gulp.dest('public/css'))
+        .pipe(reload({stream: true}));
+});
+
+// gulp.task('scripts', function() {
+// 	return gulp.src(['js/libs/*.js','js/*.js'])
+// 	.pipe(plumber())
+// 	.pipe(concat('main.js'))
+// 	.pipe(gulp.dest('build/js/'))
+// 	.pipe(reload({stream: true}));
+// });
+
+gulp.task('default',['browser-sync-dev']);
