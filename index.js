c1277065b9e6cfe8d9c74d0bcc61879491b42919
jkc621 <jameskitamirike@gmail.com>
2015-07-01 18:13:23 +0200

    Added support for notifications when user logs on or off

--- index.js
+++ index.js
@@ -11,12 +11,27 @@ app.get("/", function(req, res){
 });
 
 io.on('connection', function(socket){
-	clientSockets[socket.id] = socket;
+	clientSockets[socket.id] = {
+		socket: socket,
+		participant_name: ""
+	};
+
+	socket.on('player connected', function(data){
+		clientSockets[socket.id].participant_name = data;
+		socket.broadcast.emit('new member', data);
+	});
 
 	socket.on('chat message', function(payload){
 		io.emit('chat message', payload);
 	});
+
+	socket.on('disconnect', function(){
+		socket.broadcast.emit('player disconnected', clientSockets[socket.id].participant_name);
+		delete clientSockets[socket.id];
+	});
 });
+
+io.on
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,19 @@
 var nickname;
+var socket = io();;
+
 $(document).ready(function() {
-	$("#nickname-submit").on("click",function(e){
-		e.preventDefault();
-		nickname = $('#nickname').val();
-		$('.input-nickname').addClass('hidden');
-	})
+	socket.on("connect", function(){
+		$("#nickname-submit").on("click",function(e){
+			e.preventDefault();
+			nickname = ($('#nickname').val()).toString();
+			console.log(typeof(nickname));
+			$('.input-nickname').addClass('hidden');
+			socket.emit('player connected', nickname);		
+		});
+	});
 });
 
 $(document).ready(function() {
-	var socket = io();
 	$('#submit-button').click(function(e){
 		e.preventDefault();
 		var payload = {};
@@ -24,4 +29,19 @@ $(document).ready(function() {
 		var li = $('<li></li>').text(msg);
 		$('#messages').append(li);
 	});
+
+	socket.on('new member', function(playerName){
+		console.log(playerName);
+		var msg = playerName + " just joined this chat.";
+		var li = $('<li></li>').addClass("playerJoinedMessage").text(msg);
+		$('#messages').append(li);
+	})
+});
+
+$(document).ready(function() {
+	socket.on("player disconnected", function(data){
+		var msg = data + " left the chat.";
+		var li = $('<li></li>').addClass("playerLeftMessage").text(msg);
+		$('#messages').append(li);
+	})
 });
